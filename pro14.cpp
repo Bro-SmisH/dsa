825833ad3f438e8a72dbb060e5d346687579aafd
Bro-SmisH <you@example.com>
2022-06-30 01:06:02 +0530

    git

---
+++ pro14.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    char ch = 65;
+    while (i <= n)
+    {
+        int j = 1;
+
+        while (j <= i)
+        {
+            cout << ch << " ";
+            j++;
+            // ch++;
+        }
+        // ch = 'A' + i - 1;
+        ch++;
+        cout << endl;
+
+        i++;
+    }
+}
