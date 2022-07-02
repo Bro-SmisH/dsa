853ec94f6b3d2667bee92e5a38b16e63fbeb2dee
Bro-SmisH <you@example.com>
2022-07-02 23:03:43 +0530

    gti

--- pro24.cpp
+++ pro24.cpp
@@ -8,7 +8,19 @@ int main(){
     int i = 0;
     for (int i = 0; i < n; i++)
     {
-        
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << j+1 << " ";
+        }
+        for (int j = i-1; j >= 0; j--)
+        {
+            cout << j+1 << " ";
+        }
+        cout << endl;
     }
     
 }
