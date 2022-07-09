5f19cced2d25ace281995dc39eae59d53874b793
Bro-SmisH <you@example.com>
2022-07-09 16:53:44 +0530

    git

---
+++ pro28.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;\
+
+
+int main(){
+
+    int a;
+    int b;
+    char op;
+    cin >> a >> op >> b;
+
+
+    switch (op)
+    {
+    case '+':
+        cout << a + b;
+        break;
+    
+    case '-':
+        cout << a - b;
+        break;
+
+    case '*':
+        cout << a * b;
+        break;
+
+    case '/':
+        cout << a / b;
+        break;
+
+    case '%':
+        cout << a % b;
+        break;
+
+    default:
+        break;
+    }
+
+}
