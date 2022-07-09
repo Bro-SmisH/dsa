f34da9d381ca4ec036fb5175c4256875463f8fef
Bro-SmisH <you@example.com>
2022-07-09 16:53:52 +0530

    git

--- power_of_two.cpp
+++ power_of_two.cpp
@@ -5,25 +5,7 @@ class Solution
 public:
     bool isPowerOfTwo(int n)
     {
-        if (n == 0)
-        {
-            return false;
-        }
-        if (n == 1)
-        {
-            return true;
-        }
-
-        int temp = n;
-        while (temp != 0)
-        {
-            if (temp % 2 == 1)
-            {
-                return false;
-            }
-            temp /= 2;
-        }
-        return true;
+        
     }
 };
 
