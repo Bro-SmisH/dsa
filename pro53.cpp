a83332f9acf753f54a46108ea15f8a134d02f4c4
Bro-SmisH <shivanshmishra410@gmail.com>
2023-05-20 01:33:54 +0530

    git

--- pro53.cpp
+++ pro53.cpp
@@ -1,25 +1,8 @@
 #include <iostream>
 using namespace std;
-
-int sum(int *arr, int n)
-{
-    if (n == 0)
-    {
-        return 0;
-    }
-    int s = 0;
-    if(n == 1)
-    {
-        return arr[0];
-    }
-    s = arr[0] + sum(arr+1, n-1);
-    return s;
-    
-}
-
+#include <vector>
 int main()
 {
-    int arr[5] = {1,2,3,4,5};
-    int size = 5;
-    cout << sum(arr, size);
+    vector<int> v = {{1,2,3},{3,4,5},{2,3,4}};
+    sort(v.begin(), v.end());
 }
